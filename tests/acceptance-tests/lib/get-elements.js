ed2df9a45b1f1e11273bd4c5d727ce3672e0375e
THEozmic <michael.ozoemena@andela.com>
2017-10-02 14:08:22 +0100

    get paystack element function

--- tests/acceptance-tests/lib/get-elements.js
+++ tests/acceptance-tests/lib/get-elements.js
@@ -4,15 +4,25 @@ module.exports = {
   getElementByXpath: function (path) {
     return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
   },
-  getElementById: function (ele) {
-    const grabElement = browser.execute(function (ele) {
-      const elementStr = ele;
+  getElementById: function (element) {
+    const grabElement = browser.execute(function (element) {
+      const elementStr = element;
       const elementId = '[id^="' + elementStr + '-"]';
       return "#" + document.querySelector(elementId).id;
-    }, ele);
+    }, element);
     return grabElement;
   },
   retId: function (element) {
     return this.getElementById(element).value;
+  },
+  customGetElementById: function (element) {
+    const grabElement = browser.execute(function (element) {
+      const elementStr = element;
+      return "#" + elementStr;
+    }, element);
+    return grabElement;
+  },
+  customRetId: function (element) {
+    return this.customGetElementById(element).value;
   }
 };
