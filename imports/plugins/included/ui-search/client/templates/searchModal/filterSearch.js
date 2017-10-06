299361d716ceb78b30661bd5a9b5a051b4ead17c
Dannytebj <Dannytebj@gmail.com>
2017-10-06 07:11:26 +0100

    [Feature] Search sort Filter Incomplete

--- imports/plugins/included/ui-search/client/templates/searchModal/filterSearch.js
+++ imports/plugins/included/ui-search/client/templates/searchModal/filterSearch.js
@@ -4,6 +4,7 @@ import _ from "underscore";
 
 Template.filterSearch.helpers({
   getBrands(products) {
+    // console.log(products)
     return _.uniq(_.pluck(products, "vendor"));
   }
 });
@@ -11,15 +12,16 @@ Template.filterSearch.helpers({
 Template.filterSearch.events({
   "change #price-filter": function (event) {
     // console.log(event.target.value);
-    Session.set("priceFilter", event.target.value);
+    Session.set("filterPrice", event.target.value);
   },
-  "change #brand-filter": function (event) {
-    Session.set("brandFilter", event.target.value);
+  "change #vendor-filter": function (event) {
+    Session.set("filterVendor", event.target.value);
   }
 });
 
 Template.sortRelevance.events({
-  "change #sort-value": function (event) {
+  "change #sortRelevance": function (event) {
+    // console.log(event.target.value, 'hello world')
     Session.set("sortValue", event.target.value);
   }
 });
--- imports/plugins/included/ui-search/client/templates/searchModal/searchModal.js
+++ imports/plugins/included/ui-search/client/templates/searchModal/searchModal.js
@@ -83,8 +83,8 @@ Template.searchModal.onCreated(function () {
   this.autorun(() => {
     const searchCollection = this.state.get("searchCollection") || "products";
     const searchQuery = this.state.get("searchQuery");
-    const priceQuery = Session.get("priceFilter");
-    const brandQuery = Session.get("brandFilter");
+    const priceQuery = Session.get("filterPrice");
+    const brandQuery = Session.get("filterVendor");
     const sortQuery = Session.get("sortValue");
     const facets = this.state.get("facets") || [];
     const sub = this.subscribe("SearchResults", searchCollection, searchQuery, facets);
@@ -96,17 +96,17 @@ Template.searchModal.onCreated(function () {
        */
       if (searchCollection === "products") {
         let productResults = ProductSearch.find().fetch();
-        console.log(productResults);
+        // console.log(productResults);
         if (!["null", "all"].includes(priceQuery) && priceQuery) {
           const range = priceQuery.split("-");
           // console.log(range);
           productResults = priceFilter(productResults, range);
-          // console.log(productResults);
         }
         if (!["null", "all"].includes(brandQuery) && brandQuery) {
           productResults = brandFilter(productResults, brandQuery);
         }
         if (sortQuery !== "null" && sortQuery) {
+          console.log(sortQuery);
           productResults = sort(productResults, sortQuery);
         }
 
