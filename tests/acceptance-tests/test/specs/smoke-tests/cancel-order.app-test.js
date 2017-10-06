309a10d74d2f6e265700494af56e1f74ca2cef73
blessing <ayeniblessing101@gmail.com>
2017-10-06 11:28:06 +0100

    [feature #151208542]user is able to cancel an order

--- tests/acceptance-tests/test/specs/smoke-tests/cancel-order.app-test.js
+++ tests/acceptance-tests/test/specs/smoke-tests/cancel-order.app-test.js
@@ -13,8 +13,8 @@ beforeEach(function () {
   // });
 });
 
-xdescribe("simple login test", function () {
-  it("verify user is able to login - and verifies user name in dropdown", function () {
+describe("user", function () {
+  it("verify user is able to cancel an order", function () {
     const eleMap = yaml.safeLoad(fs.readFileSync("./tests/acceptance-tests/elements/element-map.yml", "utf8"));
     const eleIds = yaml.safeLoad(fs.readFileSync("./tests/acceptance-tests/elements/element-ids.yml", "utf8"));
     const usrData = yaml.safeLoad(fs.readFileSync("./tests/acceptance-tests/config/user-data.yml", "utf8"));
@@ -22,7 +22,6 @@ xdescribe("simple login test", function () {
     // default to process env if we've got that
     const adminEmail = process.env.REACTION_EMAIL || usrData.admin_email;
     const adminPassword = process.env.REACTION_AUTH || usrData.admin_pw;
-    const adminUserName = process.env.REACTION_USER || usrData.admin_user;
 
     browser.pause("5000");
     browser.click(eleMap.login_dropdown_btn);
@@ -31,6 +30,14 @@ xdescribe("simple login test", function () {
     browser.setValue(getId.retId(eleIds.login_pw_fld_id), adminPassword);
     browser.click(eleMap.login_btn);
     browser.pause("5000");
-    expect(browser.getText("#logged-in-display-name")).to.equal(adminUserName);
+    browser.click(eleMap.admin_dropdown_menu);
+    browser.pause("5000");
+    browser.click("a#dropdown-apps-dashboard/orders");
+    browser.pause("5000");
+    brower.click("button.btn.btn-sm.btn-danger.flex-item-fill");
+    browser.pause("5000");
+    browser.click("button.swal2-confirm swal2-styled");
+    browser.pause("5000");
+    expect(browser.getText("#order-status")).to.equal("Your order is now canceled.");
   });
 });
