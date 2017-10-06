91ccc97835bb07c47d98a2c9c14d9a861dd2c0ec
louisdante9 <louisdante9@gmail.com>
2017-10-06 16:23:43 +0100

    add product review and rating for reaction commerce

---
+++ imports/plugins/included/product-detail-simple/client/templates/review.js
@@ -0,0 +1,53 @@
+import { Template } from "meteor/templating";
+import { Meteor } from "meteor/meteor";
+import { Reaction } from "/client/api";
+import "./review.html";
+import { Reviews } from "/lib/collections";
+import { Products } from "/lib/collections";
+import { Packages } from "/lib/collections";
+import { ReactiveDict } from "meteor/reactive-dict";
+
+console.log(Products);
+const review = {};
+Template.productReviews.events({
+  "click .stars": () => {
+    const rating = $('#rating').data('userrating');
+    review.rating = rating;
+  },
+  "click #submit": () => {
+    review.comment = document.getElementById("comment").value;
+    if (review.comment === "") {
+      Alerts.toast("Sorry can't send an empty comment", "error");
+      return false;
+    }
+    const productId = () => Reaction.Router.getParam("handle");
+      document.getElementById("comment").value = "";
+    review.productId = Products.findOne(productId())._id;
+    try {
+      review.username = Meteor.user().username || Meteor.user().emails[0].address;
+      review.dateCreated = new Date;
+      Meteor.call("insert/review", review, function (error, result) {
+        console.log(error, result);
+        if (error) {
+          return error;
+        }
+        console.log(result);
+        return result;
+      });
+    }    catch (error) {
+      Alerts.toast("You need to sign in to post a review", "error");
+    }
+  }
+});
+
+
+Template.showReviews.helpers({
+  reviews: () => {
+    const productId = () => Reaction.Router.getParam("handle");
+    console.log(productId());
+    const productidentification =  Products.findOne(productId()) && Products.findOne(productId())._id;
+    debugger;
+    Meteor.subscribe("Reviews");
+    return Reviews.find({productId: productidentification}).fetch();
+  }
+});
