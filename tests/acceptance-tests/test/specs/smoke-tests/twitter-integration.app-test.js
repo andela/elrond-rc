17f61e5c6bb6dba9cf767131195de7f34eb8dbbf
Dannytebj <Dannytebj@gmail.com>
2017-10-11 13:40:18 +0100

    [Feature] Address comments - Address TTL comments - Pull Staging [Delivers #151208547]

--- tests/acceptance-tests/test/specs/smoke-tests/twitter-integration.app-test.js
+++ tests/acceptance-tests/test/specs/smoke-tests/twitter-integration.app-test.js
@@ -1,8 +1,9 @@
 "use strict";
+
 const yaml = require("js-yaml");
 const fs = require("fs");
 const expect = require("chai").expect;
-
+/* eslint-disable */
 beforeEach(function () {
   const browserConfig = yaml.safeLoad(fs.readFileSync("./tests/acceptance-tests/config/settings.yml", "utf8"));
   const baseUrl = browserConfig.base_url.toString();
@@ -20,3 +21,4 @@ describe("twitter test", function () {
     expect(browser.getAttribute("div", "twitter-integration")).to.exist;
   });
 });
+/* eslint-enable */
