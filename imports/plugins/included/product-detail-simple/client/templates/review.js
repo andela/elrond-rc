99e45bb9a79f87fa490cc10b4accd57bec4bdddc
louisdante9 <louisdante9@gmail.com>
2017-10-11 12:40:11 +0100

    refactor code for pr review feedback

--- imports/plugins/included/product-detail-simple/client/templates/review.js
+++ imports/plugins/included/product-detail-simple/client/templates/review.js
@@ -1,13 +1,12 @@
 import { Template } from "meteor/templating";
 import { Meteor } from "meteor/meteor";
 import { Reaction } from "/client/api";
-import "./review.html";
 import { Reviews } from "/lib/collections";
 import { Products } from "/lib/collections";
 import { Packages } from "/lib/collections";
 import { ReactiveDict } from "meteor/reactive-dict";
+import "./review.html";
 
-console.log(Products);
 const review = {};
 Template.productReviews.events({
   "click .stars": () => {
@@ -21,20 +20,18 @@ Template.productReviews.events({
       return false;
     }
     const productId = () => Reaction.Router.getParam("handle");
-      document.getElementById("comment").value = "";
+    document.getElementById("comment").value = "";
     review.productId = Products.findOne(productId())._id;
     try {
       review.username = Meteor.user().username || Meteor.user().emails[0].address;
       review.dateCreated = new Date;
       Meteor.call("insert/review", review, function (error, result) {
-        console.log(error, result);
         if (error) {
           return error;
         }
-        console.log(result);
         return result;
       });
-    }    catch (error) {
+    } catch (error) {
       Alerts.toast("You need to sign in to post a review", "error");
     }
   }
@@ -44,9 +41,8 @@ Template.productReviews.events({
 Template.showReviews.helpers({
   reviews: () => {
     const productId = () => Reaction.Router.getParam("handle");
-    console.log(productId());
-    const productidentification =  Products.findOne(productId()) && Products.findOne(productId())._id;
+    const productidentification = Products.findOne(productId()) && Products.findOne(productId())._id;
     Meteor.subscribe("Reviews");
-    return Reviews.find({productId: productidentification}).fetch();
+    return Reviews.find({ productId: productidentification }).fetch();
   }
 });
